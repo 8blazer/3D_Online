0e2ddf0d83f9fe2c32069f15df2383594d7cb006
Taran Zorn <diamondfellow14@gmail.com>
2021-03-17 11:57:11 -0700

    flower grow now

--- BeeRestaurant/Assets/Scripts/FlowerGrowing.cs
+++ BeeRestaurant/Assets/Scripts/FlowerGrowing.cs
@@ -5,9 +5,14 @@ using Mirror;
 
 public class FlowerGrowing : NetworkBehaviour
 {
-    [SerializeField] private float growthCheckTimer = .05f; //bigger = slower
-    //[SerializeField] private float upwardsGrowth = .01f; // rate that plant moves upwards
+     private float growthCheckTimer = .05f; //bigger = slower
 
+    [SerializeField] private float seedGrowthRate = .05f;
+    [SerializeField] private float sproutGrowthRate = .001f;
+    [SerializeField] private float flowerGrowthRate = .0001f;
+
+    [SerializeField] private float sproutStartSize;
+    [SerializeField] private float flowerStartSize;
     [SerializeField] private float seedMaxScale;
     [SerializeField] private float sproutMaxScale;
     [SerializeField] private float flowerMaxScale;
@@ -21,22 +26,17 @@ public class FlowerGrowing : NetworkBehaviour
     private int growthStage = 0; //0 seed 1 sprout 2 flower
     public bool isGrown;
     private float timer;
-
     private Vector3 sproutStartScale;
     private Vector3 flowerStartScale;
 
     [ServerCallback]
-    private void Awake()
-    {
-        sproutStartScale = sproutObject.transform.localScale;
-        flowerStartScale = flowerObject.transform.localScale;
-        flowerObject.transform.localScale = Vector3.zero;
-        sproutObject.transform.localScale = Vector3.zero;
-    }
-    [ServerCallback]
     void Start()
     {
+        flowerObject.transform.localScale = Vector3.zero;
+        sproutObject.transform.localScale = Vector3.zero;
         gameObject.tag = "Untagged";
+        sproutStartScale = new Vector3(sproutStartSize, sproutStartSize, sproutStartSize);
+        flowerStartScale = new Vector3(flowerStartSize, flowerStartSize, flowerStartSize);
     }
     [ServerCallback]
     void FixedUpdate()
@@ -49,26 +49,28 @@ public class FlowerGrowing : NetworkBehaviour
                 case 0:
                     if (seedObject.transform.localScale.x >= seedMaxScale)
                     {
-                        seedObject.transform.localScale.Set(0, 0, 0);
+                        seedObject.transform.localScale = new Vector3(0, 0, 0);
                         growthStage = 1;
                         sproutObject.transform.localScale = sproutStartScale;
                     }
                     else if (timer > growthCheckTimer)
                     {
-                        Grow(seedObject);
+                        Vector3 transfer = new Vector3(seedGrowthRate, seedGrowthRate, seedGrowthRate);
+                        seedObject.transform.localScale += transfer;
                         timer = 0;
                     }
                     break;
                 case 1:
                     if (sproutObject.transform.localScale.x >= sproutMaxScale)
                     {
-                        sproutObject.transform.localScale.Set(0, 0, 0);
+                        sproutObject.transform.localScale = new Vector3(0, 0, 0);
                         growthStage = 2;
                         flowerObject.transform.localScale = flowerStartScale;
                     }
                     else if (timer > growthCheckTimer)
                     {
-                        Grow(sproutObject);
+                        Vector3 transfer = new Vector3(sproutGrowthRate, sproutGrowthRate, sproutGrowthRate);
+                        sproutObject.transform.localScale += transfer;
                         timer = 0;
                     }
                     break;
@@ -80,19 +82,12 @@ public class FlowerGrowing : NetworkBehaviour
                     }
                     else if (timer > growthCheckTimer)
                     {
-                        Grow(flowerObject);
+                        Vector3 transfer = new Vector3(flowerGrowthRate, flowerGrowthRate, flowerGrowthRate);
+                        flowerObject.transform.localScale += transfer;
                         timer = 0;
                     }
                     break;
             }
         }
-    }
-    [Server]
-    private void Grow(GameObject growingObject)
-    {
-        //Vector3 transfer1 = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + upwardsGrowth, gameObject.transform.position.z);
-        //gameObject.transform.position = transfer1;
-        Vector3 transfer = new Vector3(.05f, .05f, .05f);
-        growingObject.transform.localScale += transfer;
     }
 }
